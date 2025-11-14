0bd9ac1e966f39f4169b092486ba14b706dc41b6
Hongwei <gmhwxi@gmail.com>
2025-11-14 12:01:00 -0500

    Updating: very very minorly

--- libraries/Library/FnTree/FnTreeSUtil.java
+++ libraries/Library/FnTree/FnTreeSUtil.java
@@ -1,6 +1,5 @@
 package Library.FnTree;
 
-
 public class FnTreeSUtil {
 //
     public static<T>
@@ -15,6 +14,28 @@ public class FnTreeSUtil {
 	FnTree<T> cons
 	(T x0, FnTree<T> lxs, FnTree<T> rxs) {
 	return new FnTree<T>(x0, lxs, rxs);
+    }
+//
+    public static<T>
+	int size(FnTree<T> xs0) {
+	if (xs0.nilq()) {
+	    return 0;
+	} else {
+	    return 1 + size(xs0.lchild()) + size(xs0.rchild());
+	}
+    }
+//
+    private static int
+	intmax(int x, int y) {
+	return (x >= y? x : y);
+    }
+    public static<T>
+	int height(FnTree<T> xs0) {
+	if (xs0.nilq()) {
+	    return 0;
+	} else {
+	    return 1 + intmax(size(xs0.lchild()), size(xs0.rchild()));
+	}
     }
 //
 } // end of [public class FnTreeSUtil{...}]
