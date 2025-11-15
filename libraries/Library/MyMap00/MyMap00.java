c87b3085d9927c65cd4f3904d6671df4f142494a
Hongwei <gmhwxi@gmail.com>
2025-11-15 00:42:27 -0500

    Updating: very very minorly

--- libraries/Library/MyMap00/MyMap00.java
+++ libraries/Library/MyMap00/MyMap00.java
@@ -15,13 +15,17 @@ public interface MyMap00<K,V> {
 //
     LnStrm<FnTupl2<K,FnList<V>>> strmize();
 //
-    FnList<V> remove$raw(K key); // HX: [key] is assumed to be in the map
-    FnList<V> remove$exn(K key); // HX: exception if [key] is not in the map
-    FnList<V> remove$opt(K key); // HX: return null if [key] is not in the map
+    FnList<V> search$raw(K key); // HX: [key] is assumed to be in the map
+    FnList<V> search$exn(K key); // HX: exception if [key] is not in the map
+    FnList<V> search$opt(K key); // HX: return null if [key] is not in the map
 //
     void insert$raw(K key, V val); // HX: insertion is assumed to work
     void insert$exn(K key, V val); // HX: exception if no insertion is done
     boolean insert$opt(K key, V val); // HX: return false if no insertion is done
+//
+    FnList<V> remove$raw(K key); // HX: [key] is assumed to be in the map
+    FnList<V> remove$exn(K key); // HX: exception if [key] is not in the map
+    FnList<V> remove$opt(K key); // HX: return null if [key] is not in the map
 //
     void foritm(BiConsumer<? super K, ? super V> work);
     // If a key mapped to multiple values, each key-value pair needs to be processed
